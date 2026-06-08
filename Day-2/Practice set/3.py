3a806f47ae4d7661b7966209bd69c56b83b26e9f
Gouri1419 <gourianadagouda@gmail.com>
2026-06-08 21:49:16 +0530

    Added Day 2 practice set Q3

---
+++ Day-2/Practice set/3.py
@@ -0,0 +1,5 @@
+# 3. Check the type of variable assigned using input() function.
+a = input("enter name:")
+print("The type of variable a is:", type(a))
+b = int(input("enter age:"))
+print("The type of variable b is:", type(b))
