9d26c6d6454bdc652c0dbc372fe4bb1e1785b383
Gouri1419 <gourianadagouda@gmail.com>
2026-06-08 21:30:24 +0530

    Added Day 2 Datatypes example

---
+++ Day-2/datatypes.py
@@ -0,0 +1,15 @@
+a = 5        # Integer
+b = 10.4     # Float
+c = "Gouri"  # String
+d = True     # Boolean
+e = None     # NoneType
+f = 1j       # Complex Number
+g = 35e3     # Scientific Notation (Float)
+
+print(type(a))  # Prints <class 'int'>
+print(type(b))  # Prints <class 'float'>
+print(type(c))  # Prints <class 'str'>
+print(type(d))  # Prints <class 'bool'>
+print(type(e))  # Prints <class 'NoneType'>
+print(type(f))  # Prints <class 'complex'>
+print(type(g))  # Prints <class 'float'>
