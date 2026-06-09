a512901015b574609c8b0b0ad65b5dd1804b53d7
Gouri1419 <gourianadagouda@gmail.com>
2026-06-09 22:17:25 +0530

    Added Day 3 practice set Q3

---
+++ Day-3/Practice set/3.py
@@ -0,0 +1,3 @@
+# 3. Write a program to detect double space in a string.
+name = input("Enter a string: ")
+print(name.find("  ")) # Output: index of first occurrence of double space, -1 if not found
